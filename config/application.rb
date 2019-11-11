77a20ed81c907d176012d1716e81c37b07ce5e5c
hadad <hadadus@gmail.com>
2019-11-11 22:43:27 +0200

    remove assets generators

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module ReactRailsApp
     config.load_defaults 5.2
 
     config.generators do |g|
+      g.assets false
       g.test_framework :rspec,
       fixtures: false,
       view_specs: false,
